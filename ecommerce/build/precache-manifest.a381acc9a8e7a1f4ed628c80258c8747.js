cdb61166099db06485470bc2d10790ac37f14e45
thao-huynh-lam <maria.sophie8790@gmail.com>
2020-04-05 20:04:20 +0300

    create an ecommerce webpage using React

---
+++ ecommerce/build/precache-manifest.a381acc9a8e7a1f4ed628c80258c8747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "099b5b03e5a93bd4b3d7",
+    "url": "/static/css/main.504ceb81.chunk.css"
+  },
+  {
+    "revision": "099b5b03e5a93bd4b3d7",
+    "url": "/static/js/main.4ce25645.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "667d1b7c7edfa6dd0272",
+    "url": "/static/js/2.af280668.chunk.js"
+  },
+  {
+    "revision": "3cf5113d3fb9c04c02db719568242024",
+    "url": "/index.html"
+  }
+];
